da33c6a12476257149218851f8f2bbaaf170caaa
Roman Lazovskiy <r.v.lazovskiy@gmail.com>
2024-09-29 05:50:20 +0000

    Add logs

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 from fastapi import FastAPI
 from scripts.api_models import Request, Response
@@ -5,28 +6,30 @@ from scripts.inference import AiHelper
 from scripts.model_config import llm, ensemble_retriever, cross_encoder
 import uvicorn
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 ai_helper = AiHelper(llm=llm, retriever=ensemble_retriever, cross_encoder=cross_encoder)
 
 app = FastAPI()
-
 
 @app.get("/")
 def index():
     return {"text": "Интеллектуальный помощник оператора службы поддержки."}
 
-
 @app.post("/predict", response_model=Response)
 async def predict(request: Request):
+    logger.info(f"Получен вопрос пользователя: {request.question}")
     result = ai_helper(request.question)
     classification_result = ai_helper.inference_classification(request.question)
+
+    logger.info(f"Ответ бота: {result}, Классификация: class_1={classification_result['class_1']}, class_2={classification_result['class_2']}")
 
     return Response(
         answer=result,
         class_1=classification_result['class_1'],
         class_2=classification_result['class_2']
     )
-
-
 
 if __name__ == "__main__":
     host = "0.0.0.0"
