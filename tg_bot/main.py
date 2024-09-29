739e5a778f3264e661b500a80a1c63f0c1b18fe6
Роман Лазовский <r.v.lazovskiy@gmail.com>
2024-09-29 10:19:34 +0500

    Add bot commands

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -2,14 +2,13 @@ import os
 import logging
 import time
 from dotenv import load_dotenv
-from telegram import Update
+from telegram import Update, BotCommand
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
-from handlers.handlers import start, handle_message
+from handlers.handlers import start, handle_message, info, help_command
 
 def main():
     load_dotenv()
-
     TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
     if not TOKEN:
         print("Ошибка: переменная окружения TELEGRAM_BOT_TOKEN не установлена")
@@ -22,8 +21,15 @@ def main():
     logger = logging.getLogger(__name__)
 
     application = ApplicationBuilder().token(TOKEN).build()
+    application.bot.set_my_commands([
+        BotCommand("start", "Начать работу с ботом"),
+        BotCommand("info", "Информация о боте и его возможностях"),
+        BotCommand("help", "Показать список команд")
+    ])
 
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('info', info))
+    application.add_handler(CommandHandler('help', help_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     while True:
